d512d631bb2add7a05f722d75276f6877197e1b1
cattenlinger <cattenlinger@outlook.com>
2026-05-23 08:32:33 +0800

    Fix the order of --help Improve --help messages

--- lib/config.lua
+++ lib/config.lua
@@ -1,23 +1,20 @@
 local arg_parse = require'utils'.arg_parse
-local search_jump_table = require'utils'.search_jump_table
 local extext = require'utils'.text
 local unpack = table.unpack
 local write  = function(...) io.stdout:write(...) end
 
 --[[
-    Process arg list accoring to the spec table
+    Process arg list according to the spec table
     A spect table is a table that storing meta info
-    aboutn options, in format:
+    about options, in format:
     
     {
         -- Definition
-        ['--option'] = {
-            desc='description';
-            size={ min_value_count, max_value_count }; 
+        {   '--option', '--alias-1', '-alias-2';
+            size={ min_value_count, max_value_count };
             set_prop={ 'prop_name', default_value };
+            desc='description';
         };
-        -- Shortcut/Redirect
-        ['-o'] = '--options';
     }
     
     size & set_prop is optional. size is for validating
@@ -26,7 +23,10 @@ local write  = function(...) io.stdout:write(...) end
 ]]--
 local function arg_process(arg_list, cfg, spec_tbl)
     local escaped, remains = {}, {}
-
+    local spec_idx = {}
+    for _, e in ipairs(spec_tbl) do
+        for _, o in ipairs(e) do spec_idx[o] = e end
+    end
     --[[
     If values list length is less than arg_min, it will exit with error.
     If arg_max is nil, it means there is no upper limit for the number of argument values,
@@ -56,7 +56,7 @@ local function arg_process(arg_list, cfg, spec_tbl)
             goto continue_ProcessEachProp
         end
 
-        local arg_specs = search_jump_table(spec_tbl, key, 3)
+        local arg_specs = spec_idx[key]
         if not arg_specs then
             table.insert(remains, props)
             goto continue_ProcessEachProp
@@ -97,31 +97,37 @@ local function arg_process(arg_list, cfg, spec_tbl)
 	return remains, escaped
 end
 
---[[
-    Print help text 
-]]--
+--[[ Print help text ]]
 local print_help do
     local function print_spec_tbl(tbl)
         local cnt, mxlen = 0, 0
         local strbuff = {}
-        for k, v in pairs(tbl) do
-            if type(v) == 'string' then goto continue; end
-            cnt = cnt + 1
-            local desc = v.desc or 'No description'
+        for _, e in ipairs(tbl) do
+            local opts = {}
+            for _, opt in ipairs(e) do table.insert(opts, opt) end
+            local k = table.concat(opts, ', ')
+            local desc = e.desc or 'No description'
             local klen = #k
             if klen > mxlen then mxlen = klen; end
-            table.insert(strbuff, { klen, k, desc })
+            if type(desc) ~= 'table' then desc = { desc } end
+            table.insert(strbuff, { klen, k, tostring(desc[1]) })
+            for i=2, #desc do table.insert(strbuff, { 0, '', tostring(desc[i]) }) end
             ::continue::
         end
         for _, i in ipairs(strbuff) do
             local klen, k, desc = unpack(i)
-            write(string.format("\t%s%s\t%s\n", k, extext.nchar(mxlen - klen), desc))
+            write(string.format("  %s%s\t%s\n", k, extext.nchar(mxlen - klen), desc))
         end
         return cnt
     end
 
+    local banner = table.concat ({
+        'A simple and dynamic http server in lua with lua-http';
+        'Usage: http-server [options]';
+        '';
+    }, '\n')
     function print_help(config, spec_meta)
-        write('Usage: http-server [options]\n')
+        write(banner)
         local spec_tbl = unpack(spec_meta)
         write('Options:\n')
         print_spec_tbl(spec_tbl)
@@ -177,50 +183,33 @@ setmetatable(DefaultConfig, { __index=__proto })
 
 local arg_meta = {
     {
-        ['--host'] = { 
-            desc = [[-h, Address to listen to, default is "localhost"]];
-            size = { 1 }, set_prop = { 'host' };
+        {   '--host', '-h'; size = { 1 }, set_prop = { 'host' };
+            desc = { 'Address to listen to, default is "localhost"'; };
         };
-        ['--port'] = {
-            desc = [[-p, Port to listen to, default is "8000"]];
-            size = { 1, 1 }, set_prop = { 'port' };
+        {   '--port', '-p'; size = { 1, 1 }, set_prop = { 'port' };
+            desc = 'Port to listen to, default is "8000"';
         };
-        ['--root'] = {
-            desc = [[-d, Web root, default is "."]];
-            size = { 1 }, set_prop = { 'root_path' };
+        {   '--root', '-d'; size = { 1 }, set_prop = { 'root_path' };
+            desc = 'Web root, default is "."';
         };
-
-        ['-d'] = '--root';
-        ['-p'] = '--port';
-        ['-h'] = '--host';
-
-        ['--handler-path'] = {
-            desc = '-dH, Handler folder path';
-            size = { 1, 1 }, set_prop = { 'handler_dir' };
+        {   '--handler-path','-s'; size = { 1, 1 }, set_prop = { 'handler_dir' };
+            desc = { 'Handler folder path';
+                     'Server will read the `.config.lua` under this path for';
+                     'request processor and error handler, see README.MD';};
         };
-        ['-dH'] = '--handler-path';
-
-        ['--debug'] = {
-            desc = '-d, Enable all debug features';
-            size = { 0 }, set_prop = { 'debug', true };
+        {   '--debug', '-d' ; size = { 0 }, set_prop = { 'debug', true };
+            desc = 'Enable all debug features';
         };
-        ['-d'] = '--debug';
-
-        ['--log-level'] = {
-            desc = '-v, Set log level, 0-5 or ERROR,WARN,INFO,DEBUG,TRACE';
-            size = { 1 }, set_prop = { 'log_level', 'TRACE' };
+        {   '--log-level', '-v'; size = { 1 }, set_prop = { 'log_level', 'TRACE' };
+            desc = { 'Set log level, 0 to 5 or ERROR,WARN,INFO,DEBUG,TRACE';
+                     '0 means disable.' };
         };
-        ['-v'] = '--log-level';
-
-        ['--log-color'] = {
-            desc = '-lC, use colored log';
-            size = {0}, set_prop = { 'log_color', true };
+        {   '--log-color', '-vC'; size = { 0 }, set_prop = { 'log_color', true };
+            desc = { 'Use colored log';
+                     'If the TERM is not colored, this options will be ignored.'};
         };
-        ['-lC'] = '--log-color';
-
-        ['--help'] = {
+        {   '--help'; size = { 0 }, set_prop = { 'help', true };
             desc = 'Show help and exit';
-            size = { 0 }, set_prop = { 'help', true };
         };
     };
     function (config)
